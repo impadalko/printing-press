9dc8143ca2071811d57b0d9bf4839f0ba52b74af
Ivan Padalko <ivan.monteiro.padalko@gmail.com>
2021-03-24 09:07:44 -0300

    Simplify parser logic to a single iteration

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -16,50 +16,37 @@ class ParsingError extends Error {
 
 export const parseFile = async (path: string, headerDelimiter = '+++'): Promise<ParsedFile> => {
   const fileStream = fs.createReadStream(path)
-  const linesInterface = readline.createInterface({
+  const lines = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity
   })
 
-  const lines = []
-  for await (const line of linesInterface)
-    lines.push(line)
-
-  let header = ''
-  const closedHeader = lines.some((line, index) => {
-    if(index === 0 && line !== headerDelimiter)
-      throw new ParsingError('First line must be a header delimiter')
-    if(index !== 0 && line === headerDelimiter)
-      return true
-    if(line !== headerDelimiter){
-      header += `${line}\n`
-      return false
-    }
-  })
-
-  if(!closedHeader)
-    throw new ParsingError('Header was never closed')
-
-  let parsedHeader = null
-  try {
-    parsedHeader = YAML.parse(header)
-  } catch (e) {
-    throw new ParsingError('Header is not a valid YAML')
-  }
-
   let delimiterCount = 0
+  let header = ''
   const content: string[] = []
-  lines.forEach((line) => {
+  for await (const line of lines) {
+    if (delimiterCount === 0 && line !== headerDelimiter)
+      throw new ParsingError('First line must be a header delimiter')
     if (delimiterCount < 2){
       if (line === headerDelimiter)
         delimiterCount++
+      else
+        header += `${line}\n`
+      continue
     }
-    else
-      content.push(line)
-  })
+    content.push(line)
+  }
 
-  return {
-    header: parsedHeader,
-    content: content
+  if(delimiterCount === 1)
+    throw new ParsingError('Header was never closed')
+
+  try {
+    const parsedHeader = YAML.parse(header)
+    return {
+      header: parsedHeader,
+      content: content
+    }
+  } catch (e) {
+    throw new ParsingError('Header is not a valid YAML')
   }
 }
