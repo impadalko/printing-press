f6178cb56e488900e483d195a874a1ab78e4ecb6
Ivan Padalko <ivan.monteiro.padalko@gmail.com>
2021-03-31 19:09:04 -0300

    Add support for strikethrough text

--- src/lib/contentConverter/HTML/formatter.ts
+++ src/lib/contentConverter/HTML/formatter.ts
@@ -6,15 +6,19 @@ export const format = (line: string): string => {
   while ((match = boldRegex.exec(line))) {
     line = line.replace(match[0], `<bold>${match[1]}</bold>`)
   }
-  const italicRegex = /\*([^**]+)\*/g
+  const italicRegex = /\*([^*]+)\*/g
   while ((match = italicRegex.exec(line))) {
     line = line.replace(match[0], `<em>${match[1]}</em>`)
+  }
+  const strikedRegex = /~~([^~~]+)~~/g
+  while ((match = strikedRegex.exec(line))) {
+    line = line.replace(match[0], `<s>${match[1]}</s>`)
   }
   const codeRegex = /`([^`]+)`/g
   while ((match = codeRegex.exec(line))) {
     line = line.replace(match[0], `<code>${match[1]}</code>`)
   }
-  const linkRegex = /\[(.+)\]\((.+)\)/g
+  const linkRegex = /\[([^[\]]+)\]\(([^()]+)\)/g
   while ((match = linkRegex.exec(line))) {
     line = line.replace(match[0], `<a href="${match[2]}">${match[1]}</a>`)
   }
--- tests/lib/contentConverter/HTML/formatter.test.ts
+++ tests/lib/contentConverter/HTML/formatter.test.ts
@@ -1,19 +1,25 @@
 import { format } from '../../../../src/lib/contentConverter/HTML/formatter'
 
-test('Format bold, italic, code and links correctly', () => {
+test('Format bold, italic, strikethrough, code and links correctly', () => {
   let result = format('**abc**')
   expect(result).toBe('<bold>abc</bold>')
   result = format('*abc*')
   expect(result).toBe('<em>abc</em>')
+  result = format('~~abc~~')
+  expect(result).toBe('<s>abc</s>')
   result = format('`abc`')
   expect(result).toBe('<code>abc</code>')
   result = format('http://example.com')
   expect(result).toBe('<a href="http://example.com">http://example.com</a>')
   result = format('[Example](http://example.com)')
   expect(result).toBe('<a href="http://example.com">Example</a>')
-  result = format('*i1* **b1** `c1` **b2** [URL](https://url.com) `c2` https://url2.com *i2*')
+  result = format('*i1* **b1** **b2** *i2*')
+  expect(result).toBe('<em>i1</em> <bold>b1</bold> <bold>b2</bold> <em>i2</em>')
+  result = format(
+    '[URL1](https://url1.com) https://url2.com https://url3.com [URL4](https://url4.com)'
+  )
   expect(result).toBe(
-    '<em>i1</em> <bold>b1</bold> <code>c1</code> <bold>b2</bold> <a href="https://url.com">URL</a> ' +
-      '<code>c2</code> <a href="https://url2.com">https://url2.com</a> <em>i2</em>'
+    '<a href="https://url1.com">URL1</a> <a href="https://url2.com">https://url2.com</a> ' +
+      '<a href="https://url3.com">https://url3.com</a> <a href="https://url4.com">URL4</a>'
   )
 })
