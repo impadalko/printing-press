b47001f45b7914bd2494e184e8e2d5d0eb07ba9e
Ivan Padalko <ivan.monteiro.padalko@gmail.com>
2021-05-23 16:08:30 -0300

    Fix error on one file crashing the whole process

--- src/api/index.ts
+++ src/api/index.ts
@@ -29,34 +29,38 @@ const buildContent = async (
 
   await Promise.all(
     contentFiles.map(async (file) => {
-      const parsedFile = await parseFile(file)
+      try {
+        const parsedFile = await parseFile(file)
 
-      const template = parsedFile.header?.template || buildOptions.defaultTemplate
-      if (!template) return
+        const template = parsedFile.header?.template || buildOptions.defaultTemplate
+        if (!template) return
 
-      if (!loadedTemplates[template]) {
-        const templateFilePath = path.join(templatePath, template)
-        if (!(await exists(templateFilePath))) {
-          console.log(`${file}: Cannot find ${template} in template files`)
-          return
+        if (!loadedTemplates[template]) {
+          const templateFilePath = path.join(templatePath, template)
+          if (!(await exists(templateFilePath))) {
+            console.error(`${file}: Cannot find ${template} in template files`)
+            return
+          }
+          loadedTemplates[template] = await fs.readFile(templateFilePath, { encoding: 'utf-8' })
         }
-        loadedTemplates[template] = await fs.readFile(templateFilePath, { encoding: 'utf-8' })
-      }
 
-      let output = loadedTemplates[template]
-      let match: RegExpExecArray | null = null
-      while ((match = delimiterRegex.exec(loadedTemplates[template]))) {
-        if (match[1] === 'content') output = output.replace(match[0], convert(parsedFile.content))
-        else if (parsedFile.header?.[match[1]])
-          output = output.replace(match[0], parsedFile.header?.[match[1]])
-        else console.log(`${file}: Could not find ${match[1]} in header`)
-      }
+        let output = loadedTemplates[template]
+        let match: RegExpExecArray | null = null
+        while ((match = delimiterRegex.exec(loadedTemplates[template]))) {
+          if (match[1] === 'content') output = output.replace(match[0], convert(parsedFile.content))
+          else if (parsedFile.header?.[match[1]])
+            output = output.replace(match[0], parsedFile.header?.[match[1]])
+          else console.log(`${file}: Could not find ${match[1]} in header`)
+        }
 
-      const outputFilePath = path
-        .join(outputPath, path.relative(contentPath, file))
-        .replace(/\.[^.]+$/, '.html')
-      await ensureDir(outputFilePath)
-      await fs.writeFile(outputFilePath, output)
+        const outputFilePath = path
+          .join(outputPath, path.relative(contentPath, file))
+          .replace(/\.[^.]+$/, '.html')
+        await ensureDir(outputFilePath)
+        await fs.writeFile(outputFilePath, output)
+      } catch (e) {
+        console.error(`Something went wrong when processing ${file}: ${e}`)
+      }
     })
   )
 }
